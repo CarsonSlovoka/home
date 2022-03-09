75877b92b621bd43fb7c0f752cf118f54645fc04
Carson <jackparadise520a@gmail.com>
2022-03-09 18:09:25 +0800

    首次嘗試利用text/template來建立 html 模板 (#2)

--- src/main.go
+++ src/main.go
@@ -2,11 +2,13 @@ package main
 
 import (
 	io2 "carson.io/pkg/io"
+	"carson.io/pkg/tpl/funcs"
 	"log"
 	"os"
 	"path/filepath"
 	"regexp"
 	"strings"
+	"text/template"
 )
 
 type Config struct {
@@ -23,10 +25,34 @@ func init() {
 			`url\\static\\img\\.*\.md`,
 			`url\\static\\js\\.*\.md`,
 			`url\\static\\sass\\.*`,
-			`url\\tmpl\\README\.md`,
+			`url\\tmpl\\.*`, // 樣版在release不需要再給，已經遷入到source之中
 		},
 	}
+}
 
+func render(src, dst string) error {
+	dstFile, err := os.Create(dst)
+	if err != nil {
+		return err
+	}
+
+	tmplDir := "url/tmpl"
+	parseFiles := []string{src}
+	for _, filename := range []string{"head", "navbar"} {
+		parseFiles = append(parseFiles, filepath.Join(tmplDir, filename+".gohtml"))
+	}
+
+	t := template.Must(
+		template.New(filepath.Base(src)).
+			Funcs(funcs.GetUtilsFuncMap()).
+			ParseFiles(parseFiles...),
+	)
+	context := struct {
+		Version string
+	}{
+		"0.0.0",
+	}
+	return t.Execute(dstFile, context)
 }
 
 func main() {
@@ -90,6 +116,14 @@ func main() {
 		for _, src := range filePathList {
 			dst := filepath.Join("../docs/", strings.Replace(src, "url\\", "", 1))
 			// fmt.Println(dst)
+			if filepath.Ext(dst) == ".gohtml" {
+				dst = dst[:len(dst)-6] + "html"
+				if err = render(src, dst); err != nil {
+					panic(err)
+				}
+				continue
+			}
+
 			if err = io2.CopyFile(src, dst); err != nil {
 				panic(err)
 			}
